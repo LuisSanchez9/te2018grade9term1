7fde34fd4840d727f5fdc29920cc41a1f97b7710
LuisSanchez <luiss@csedge.org>
2018-10-06 13:18:02 -0700

    updated data.js

--- data.js
+++ data.js
@@ -1,5 +1,23 @@
 // Example
-let message = "Hello World!";
+let message = "Hello luis!";
 console.log(message);
+let bDayMonth=7
+let nDay=15
+let age=15
+let siblings=1
+let people= "help me"
+let 
 
-// Type your code below!
+
+
+
+
+
+
+
+
+
+
+
+
+// Type your code below
